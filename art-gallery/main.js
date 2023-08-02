34dd1cf7c118e9d16868449e9ff9b955a09e4794
Silverio Rodriguez Alcorta <silverio.rodriguez@kuarxis.com>
2023-08-01 23:08:07 -0400

    Adding paintings to the walls

--- art-gallery/main.js
+++ art-gallery/main.js
@@ -1,5 +1,6 @@
 //console.log('Three object here', THREE)
-
+import * as THREE from 'three'
+import {PointerLockControls} from 'three-stdlib'
 /* 
 Start with creating a Canvas.
 Create the scene.
@@ -47,10 +48,10 @@ document.body.appendChild(renderer.domElement)
 
 //Add some lights to light the scene
 //First parameter is color, then intensity, and the 3rd (not used here), is the distance and the 4th is the decay.
-const ambientLight = new THREE.AmbientLight(0xdddddd, 1.0)
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.0)
 
 //the light will follow the camera
-ambientLight.position = camera.position 
+//ambientLight.position = camera.position 
 
 //Add the light to the scene
 scene.add(ambientLight)
@@ -100,7 +101,7 @@ BoxGeometry is the shape of the object, parameters are width and height. You can
  * To add a texture first you have to load it with the TextureLoader function
  */
 //Texture for the floor
-const floorTexture = new THREE.TextureLoader().load('img/Floor.jpg') //this code worked for me but not for the trainer, he changed it to use ImageUtils instead
+const floorTexture = new THREE.TextureLoader().load('src/public/img/Floor.jpg') //this code worked for me but not for the trainer, he changed it to use ImageUtils instead
 //let floorTexture = new THREE.ImageUtils.loadTexture('img/Floor.jpg')//When I tried the ImageUtils it didn't worked for me only for the trainer, I went back to TextureLoader.
 
 floorTexture.wrapS = THREE.RepeatWrapping  //repeat on the horizontal direction.
@@ -132,7 +133,7 @@ scene.add(wallGroup)
 //Create the front wall
 const frontWall = new THREE.Mesh(
     new THREE.BoxGeometry(50, 20, 0.001), //This receives 3 parameters, width, height and dept. For wall we need depth to be thin so it is a wall and not a box, so dept is 0.001.
-    new THREE.MeshBasicMaterial({color: 'green'}),
+    new THREE.MeshLambertMaterial({color: 'green'}),
 )
 
 frontWall.position.z = -20 //Move the front wall back to is not to close to the camara and to give more space in the room. 
@@ -140,7 +141,7 @@ frontWall.position.z = -20 //Move the front wall back to is not to close to the 
 //Create the left wall
 const leftWall = new THREE.Mesh(
     new THREE.BoxGeometry(50, 20, 0.001),
-    new THREE.MeshBasicMaterial ({
+    new THREE.MeshLambertMaterial ({
         color: 'red'
     })
 )
@@ -152,7 +153,7 @@ leftWall.position.x = -20
 //Right Wall
 const rightWall = new THREE.Mesh(
     new THREE.BoxGeometry (50, 20, 0.001),
-    new THREE.MeshBasicMaterial ({
+    new THREE.MeshLambertMaterial ({
         color: 'yellow'
     })
 )
@@ -163,7 +164,7 @@ rightWall.position.x = 20
 
 //Create the ceiling
 const ceilingGeometry = new THREE.PlaneGeometry(50, 50) //BoxGeometry is the shape of the object
-const ceilingMaterial =  new THREE.MeshBasicMaterial({
+const ceilingMaterial =  new THREE.MeshLambertMaterial({
     color: 'blue'
 })
 
@@ -176,7 +177,40 @@ scene.add(ceilingPlane)
 
 wallGroup.add(frontWall, leftWall, rightWall)
 
+function createPainting(imageUrl, width, height, position){
+    const textureLoader = new THREE.TextureLoader()
+    const paintingTexture = textureLoader.load(imageUrl)
+    const paintingMaterial = new THREE.MeshBasicMaterial({
+        map: paintingTexture,
+    })
+    const paintingGeometry = new THREE.PlaneGeometry(width, height)
+    const painting = new THREE.Mesh(paintingGeometry, paintingMaterial)
+    painting.position.set(position.x, position.y, position.z)
+    return painting
+}
 
+const painting1 = createPainting(
+    './src/public/img/1656037107636.jpg',
+    10,
+    5,
+    new THREE.Vector3(-10, 5, -19.99)
+    )
+
+const painting2 = createPainting(
+        './src/public/img/Fireworks.jpg',
+        10,
+        5,
+        new THREE.Vector3(10, 5, -19.99)
+        )
+
+//Loop through each wall and create the bounding box
+//The bounding box is for creating a colider so the person walking in the room colide with the walls and cannot pass thru the walls.
+// for (let i = 0; i < wallGroup.children.length; i++) {
+//     wallGroup.children[i].BBox = new THREE.Box3()
+//     wallGroup.children[i].BBox.setFromObject(wallGroup.children[i])
+// }
+
+scene.add(painting1, painting2)
 
 //Function when a key is pressed, execute this function
 function onKeyDown(event) {
